cf0caca6d811916e0845005cefe3e92ceb0fff14
Frank Denis <github@pureftpd.org>
2026-05-27 13:59:09 +0200

    Update for zig-current

--- build.zig
+++ build.zig
@@ -29,4 +29,24 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .link_libc = true,
     });
+
+    const test_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    test_mod.addCSourceFile(.{
+        .file = b.path("tests/tests.c"),
+    });
+    test_mod.addIncludePath(b.path("."));
+    test_mod.linkLibrary(lib);
+
+    const test_exe = b.addExecutable(.{
+        .name = "hydrogen-tests",
+        .root_module = test_mod,
+    });
+
+    const run_tests = b.addRunArtifact(test_exe);
+    const test_step = b.step("test", "Run the test suite");
+    test_step.dependOn(&run_tests.step);
 }
